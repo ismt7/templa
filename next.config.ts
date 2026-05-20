fde1e6c9df4dc9fa03450f8e8d5c325374f329e5
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2026-05-20 11:33:53 +0900

    Remove unused rewrites from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,6 @@ import { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   images: { unoptimized: true },
-  async rewrites() {
-    return [
-      {
-        source: "/templa",
-        destination: "/",
-      },
-      {
-        source: "/templa/:path*",
-        destination: "/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
